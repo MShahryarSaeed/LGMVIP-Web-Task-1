657217037ea2f35d07c4024b49bb16e5f5772006
Shahryar <shahryarsaeed06@gmail.com>
2023-09-11 00:54:11 +0500

    To-Do-List

---
+++ To-Do-List/script.js
@@ -0,0 +1,43 @@
+const ListContainer=document.getElementById("list-container");
+const inputBox=document.getElementById("input-box");
+
+//  Add Task
+const addTask=()=>{
+
+    if(inputBox.value===''){
+        alert("Enter Some Data");
+    }
+    else{
+        let li=document.createElement("Li");
+        li.innerHTML=inputBox.value;
+        ListContainer.appendChild(li);
+
+        let span=document.createElement("span");
+        span.innerHTML="\u00d7";
+        li.appendChild(span);
+    }
+
+    inputBox.value="";
+    saveTask();
+}
+
+ListContainer.addEventListener("click",function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("checked");
+        saveTask();
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+        saveTask();
+    }
+});
+
+function saveTask(){
+    localStorage.setItem("data",ListContainer.innerHTML);
+}
+
+function showTask(){
+    ListContainer.innerHTML=localStorage.getItem("data");
+}
+
+showTask();
